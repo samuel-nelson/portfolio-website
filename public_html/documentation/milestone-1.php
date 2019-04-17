8b250a6a8555eb334294a7679ea3f30dd950e962
Samuel Nelson <snelson54@cnm.edu>
2019-04-17 14:31:44 -0600

    Updated formatting

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,45 +7,45 @@
 	<body>
 		<h2>Purpose, Audience & Goal</h2>
 			<p><b>Purpose: </b>This project is a professional portfolio website demonstrating my development
-					skills and projects that I have created.</p>
+				skills and projects that I have created.</p>
 			<p><b>Audience: </b>This site is targeted to potential employers as well as other develops for employment
-					and networking purposes and is a showcase of my skills. Target audience: Startups, hiring managers,
-					recruiters, and fellow developers.</p>
+				and networking purposes and is a showcase of my skills. Target audience: Startups, hiring managers,
+				recruiters, and fellow developers.</p>
 			<p><b>Goal: </b>The goal of this website is to demonstrate my skills to potential employers or those who may
-					want to collaborate on a project. The goal is to have contact through either the contact form on the
-					website, or through links to my GitHub or LinkedIn. The second goal is to positively contribute to my
-					online presence.</p>
+				want to collaborate on a project. The goal is to have contact through either the contact form on the
+				website, or through links to my GitHub or LinkedIn. The second goal is to positively contribute to my
+				online presence.</p>
 		<h2>Persona</h2>
 			<p><b>Name: </b>Jeffery H. Donaldson</p>
 			<p><b>Age: </b>29</p>
 			<p><b>Profession: </b>Lead Hiring Manager at Rural Sourcing in Albuquerque, NM. Rural Sourcing is looking to
-					hire a full-time full-stack web developer to work at their location on Silver and 8th Street.
-					The company focuses on creating and managing web applications for small to medium sized business,
-					providing a reliable service that is affordably priced. Jeffery is the lead manager charged with
-					maintaining the proper amount of developers to continue operations.</p>
+				hire a full-time full-stack web developer to work at their location on Silver and 8th Street.
+				The company focuses on creating and managing web applications for small to medium sized business,
+				providing a reliable service that is affordably priced. Jeffery is the lead manager charged with
+				maintaining the proper amount of developers to continue operations.</p>
 			<p><b>Technology: </b>Widows and Linux (Ubuntu) power user. Uses Lenevo Thinkpad X1 Carbon (14”)
-					(Intel Core i5, 16gb RAM, 500gb SSD, dual booting Windows 10, and Ubuntu 18.10). In the office, he
-					primarily uses his company issued HP Omen Desktop computer (Intel Core i7, 16gb RAM, 1TB HHD, 256gb SSD,
-					primarily running windows 10, but is a Hackintosh and can run MacOS X Mojave with a few glitches).
-					He has 3 Samsung SF350 Series 24” LED Monitors.  His phone is a OnePlus 6T (Snapdragon 845, 8GB RAM,
-					6.4” AMOLED Display, 16MP and 20MP Cameras, 256GB Storage, running Android OxygenOS).</p>
+				(Intel Core i5, 16gb RAM, 500gb SSD, dual booting Windows 10, and Ubuntu 18.10). In the office, he
+				primarily uses his company issued HP Omen Desktop computer (Intel Core i7, 16gb RAM, 1TB HHD, 256gb SSD,
+				primarily running windows 10, but is a Hackintosh and can run MacOS X Mojave with a few glitches).
+				He has 3 Samsung SF350 Series 24” LED Monitors.  His phone is a OnePlus 6T (Snapdragon 845, 8GB RAM,
+				6.4” AMOLED Display, 16MP and 20MP Cameras, 256GB Storage, running Android OxygenOS).</p>
 			<p><b>Attitudes and Behaviors: </b>Jeffery has to deal with an average of a 2.4 year turnover rate at
-					Rural Sourcing, primarily due to the lower wages compared to tech companies in Texas and California.
-					He often finds that entry level employees gain resume boosting experience and then move on to other
-					Tech companies. Although frustrating, the company is acknowledges that this will happen. They only pick
-					the top talent of entry-level web developers. Experience is not required, but having killer skills are.
-					The company must have efficient developers in order to turn a profit. Jeffery is willing to spend
-					significant time finding the right person for the job, because each hire significantly effects the RSI’s
-					bottom line. Jeffery averages a 40 hour week, preferring to work from home most days. He spends his time
-					reaching out to local talent on LinkedIn, reading through local developers code on GitHub, and browsing
-					portfolio websites of developers he finds that have well written code.</p>
+				Rural Sourcing, primarily due to the lower wages compared to tech companies in Texas and California.
+				He often finds that entry level employees gain resume boosting experience and then move on to other
+				Tech companies. Although frustrating, the company is acknowledges that this will happen. They only pick
+				the top talent of entry-level web developers. Experience is not required, but having killer skills are.
+				The company must have efficient developers in order to turn a profit. Jeffery is willing to spend
+				significant time finding the right person for the job, because each hire significantly effects the RSI’s
+				bottom line. Jeffery averages a 40 hour week, preferring to work from home most days. He spends his time
+				reaching out to local talent on LinkedIn, reading through local developers code on GitHub, and browsing
+				portfolio websites of developers he finds that have well written code.</p>
 			<p><b>Frustrations and Needs:</b> Jeffery needs to hire two full-time full-stack web developers for their
-					Analyst 1 portions that became available this week that need to be filled within the next 3 weeks.
-					Most developers tend to want to move out of State, so most of the top talent ends up leaving the
-					Albuquerque area. Jeffery must find the best that are still around or who are willing to relocate.</p>
+				Analyst 1 portions that became available this week that need to be filled within the next 3 weeks.
+				Most developers tend to want to move out of State, so most of the top talent ends up leaving the
+				Albuquerque area. Jeffery must find the best that are still around or who are willing to relocate.</p>
 			<p><b>Goals: </b>Jeffery's goals are to find a talented full-stack web developer to hire within the next
-					3 weeks. The develops need to write clean code and be able to fit in well with company culture.</p>
+				3 weeks. The develops need to write clean code and be able to fit in well with company culture.</p>
 			<p><b>User Story: </b>As a user, I need to determine quickly whether this person is a candidate for the
-					entry-level full-stack analyst 1 position.</p>
+				entry-level full-stack analyst 1 position.</p>
 	</body>
 </html>
