99b033276e8804b210b977dbb87ab7dd9468d06a
Samuel Nelson <snelson54@cnm.edu>
2019-04-25 16:35:29 -0600

    Added images and fixed typos.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,10 +7,11 @@
 	<body>
 		<h1>Content Strategy</h1>
 		<p>My website will be a dynamic one page portfolio and consist of the following:</p>
-		<p><strong>Welcome: </strong>A brief introduction consisting of my name and job title. This welcome will be overlayed on a background photo.</p>
-		<p><strong>About Me: </strong>This section will describe my academic history and languages I know with icons.
-			The tone of voice will be personable and could be described as business casual. The goal of the overall language and feel of the website will help describe myself to potential employers.</p>
+		<p><strong>Welcome: </strong>A brief introduction consisting of my name and job title. This welcome will be overlaid on a background photo.</p>
+		<p><strong>About Me: </strong>This section will describe my academic history and languages I know with icons. The tone of voice will be personable and could be described as business casual. The goal of the overall language and feel of the website will help describe myself to potential employers.</p>
 		<p><strong>Portfolio: </strong>This section will contain 4-6 examples of my best work. Each thumbnail will open up the example in a new tab.</p>
 		<p><strong>Contact: </strong>A simple contact form that sends content to my professional email address. In the footer section will be links to my GitHub, LinkedIn, and StackOverflow profiles.  </p>
+		<img src="assets/desktop.png" alt="How the website will look on desktop.">
+		<img src="assets/mobile.png" alt="How the website will look on mobile.">
 	</body>
 </html>
